55615c57f75ac1baea5f97186181ab4118963e47
Anish Naphade <111562498+AnishNaphade@users.noreply.github.com>
2024-09-17 09:50:11 +0530

    Add files via upload

---
+++ 20containsdup.java
@@ -0,0 +1,24 @@
+
+import java.util.HashMap;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        //Iterate through Array
+        for(int i = 0; i< nums.length; i++) {
+            if(map.containsKey(nums[i])){
+                int lastindex = map.get(nums[i]);
+                if(i - lastindex <= k){
+                    return true;
+                }
+
+
+            }
+            map.put(nums[i] , i);
+        }
+        return false;
+       
+        
+    } 
+}
