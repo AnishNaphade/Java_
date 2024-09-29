59447d6245d27854dd6a1f466e90027f83b62386
Anish Naphade <111562498+AnishNaphade@users.noreply.github.com>
2024-09-29 17:53:14 +0530

    Add files via upload

---
+++ 23removedupli.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 0) return 0;
+
+        int ptr = 1;
+        for(int i = 1; i< nums.length; i++){
+            if(nums[i] != nums[i - 1]){
+                nums[ptr] = nums[i];
+                
+                ptr++;
+
+            }  
+                    
+    }
+    return ptr; 
+    
+}
+}
